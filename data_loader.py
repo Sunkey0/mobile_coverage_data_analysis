5bb46ff75b5f86322d4ce6b109aa9644f0fe1b9a
Daniel <Daniel.londonop9@gmail.com>
2025-02-21 13:23:43 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,18 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 def load_data():
-    uploaded_file = st.sidebar.file_uploader("⬆️ Sube tu archivo CSV", type=["csv"])
-    if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
-        data.columns = [
-            'AÑO', 'TRIMESTRE', 'PROVEEDOR', 'COD_DEPARTAMENTO', 'DEPARTAMENTO', 'COD_MUNICIPIO',
-            'MUNICIPIO', 'CABECERA_MUNICIPAL', 'COD_CENTRO_POBLADO', 'CENTRO_POBLADO',
-            'COBERTURA_2G', 'COBERTURA_3G', 'COBERTURA_HSPA+', 'COBERTURA_4G', 'COBERTURA_LTE', 'COBERTURA_5G'
-        ]
-        return data
-    else:
-        st.warning("Por favor, sube un archivo CSV para continuar.")
-        st.stop()
+    # URL del archivo CSV en el repositorio de GitHub
+    url = "https://raw.githubusercontent.com/tu_usuario/tu_repositorio/main/ruta/al/archivo.csv"
+    
+    # Cargar datos desde la URL
+    data = pd.read_csv(url)
+    
+    # Renombrar columnas
+    data.columns = [
+        'AÑO', 'TRIMESTRE', 'PROVEEDOR', 'COD_DEPARTAMENTO', 'DEPARTAMENTO', 'COD_MUNICIPIO',
+        'MUNICIPIO', 'CABECERA_MUNICIPAL', 'COD_CENTRO_POBLADO', 'CENTRO_POBLADO',
+        'COBERTURA_2G', 'COBERTURA_3G', 'COBERTURA_HSPA+', 'COBERTURA_4G', 'COBERTURA_LTE', 'COBERTURA_5G'
+    ]
+    
+    return data
