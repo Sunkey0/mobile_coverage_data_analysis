14b662d509abf581843b3f0d4578cb85ca70de1f
Daniel <Daniel.londonop9@gmail.com>
2025-02-21 16:01:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from mapa_calidad_conectividad import page_mapa_calidad_conectividad
 def main():
     # Configuración inicial
     st.set_page_config(
-        page_title="Hacia una Antioquia Conectada: Diagnóstico de la cobertura móvil Urbana-Rural de Antioquia en el trimestre 3 del año 2023",
+        page_title="Hacia una Antioquia Conectada",
         page_icon="📊",
         layout="wide",
         initial_sidebar_state="expanded"  # Asegura que la barra lateral esté expandida
     )
 
     # Título de la aplicación
-    st.title("📊 Hacia una Antioquia Conectada")
+    st.title("📊 Hacia una Antioquia Conectada: Diagnóstico de la cobertura móvil Urbana-Rural de Antioquia en el trimestre 3 del año 2023")
 
     # Cargar datos
     try:
