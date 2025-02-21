9bf8f56f393ebd07baacdeff7c8ff30b84467c0b
Daniel <Daniel.londonop9@gmail.com>
2025-02-21 12:48:11 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def setup_app():
+    st.set_page_config(page_title="Hacia una Antioquia Conectada", layout="wide")
+    st.title("📊 Hacia una Antioquia Conectada: Diagnóstico de la Cobertura Móvil Urbana-Rural en el Trimestre 3 de 2023")
